276f053448cd29626f9f7b1c7ebeb2a885bb71b1
Qichen Huang <dannyqichenhuang@gmail.com>
2026-03-21 20:41:01 -0400

    chore: scaffold Astro project

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://your-username.github.io', // TODO: replace with actual GitHub username
+  base: '/my-openrg',
+  output: 'static',
+});
